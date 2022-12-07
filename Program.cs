9f6d54f1f2386c13487ba7fae9a64d7753e344e9
«Roman <research777@mail.ru>
2022-12-07 20:05:24 +0300

    SEMINAR8_Ex1_Correct

--- Program.cs
+++ Program.cs
@@ -4,23 +4,29 @@ int [,] Create2dArray (int rows, int columns, int min, int max)
 {
     int [,] newArray = new int [rows, columns];
     for (int i = 0; i < rows; i++)
+    {
         for(int j = 0; j < columns; j++)
-            newAraay[i, j] = new Random().Next(min, max+1);
+            newArray[i, j] = new Random().Next(min, max+1);
+    }
     return newArray;
 }
 
 void ShowArray(int [,] array)
 {
     for (int i = 0; i < array.GetLength(0); i++)
+    {
         for(int j = 0; j < array.GetLength(1); j++)
-            Console.Write(newAraay[i, j] + " ");
+        {
+            Console.Write(array[i, j] + " ");
+        }
         Console.WriteLine();
+    }
     Console.WriteLine(); 
 }
 
 int [,] ChangeArrayRows(int [,] array1, int row1, int row2)
 {
-    if (row1 >= 0 && row1 >= array1.GetLength(0) && row2 >= 0 && row2 >= array1.GetLength(0) && row1 != row2)
+    if (row1 >= 0 && row1 < array1.GetLength(0) && row2 >= 0 && row2 < array1.GetLength(0) && row1 != row2)
     {
         for (int j = 0; j < array1.GetLength(1); j++)
         {
@@ -30,7 +36,7 @@ int [,] ChangeArrayRows(int [,] array1, int row1, int row2)
         }
     }
     else Console.WriteLine("incorrect input!");
-    return array1;
+return array1;
 }
 
 Console.WriteLine("Input numbers of rows: ");
@@ -45,3 +51,7 @@ Console.WriteLine("Input number of 1st row for remove: ");
 int userRow1 = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Input number of 2nd row for remove: ");
 int userRow2 = Convert.ToInt32(Console.ReadLine());
+
+int [,] createdArray = Create2dArray(userRows, userColumns, userMinValue, userMaxValue);
+ShowArray (createdArray);
+ShowArray (ChangeArrayRows(createdArray, userRow1, userRow2));
