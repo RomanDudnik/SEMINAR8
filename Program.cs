787d1c3d3f536c7e298c113d495dfd8090791959
«Roman <research777@mail.ru>
2022-12-07 21:52:46 +0300

    SEMINAR8_Ex1_Correct

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿// Задайте двухмерный массив. Напишите программу которая поменяет местами две любые строки массива.
 
+/*
 int [,] Create2dArray (int rows, int columns, int min, int max)
 {
     int [,] newArray = new int [rows, columns];
@@ -51,6 +52,69 @@ Console.WriteLine("Input number of 1st row for remove: ");
 int userRow1 = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Input number of 2nd row for remove: ");
 int userRow2 = Convert.ToInt32(Console.ReadLine());
+
+int [,] createdArray = Create2dArray(userRows, userColumns, userMinValue, userMaxValue);
+ShowArray (createdArray);
+ShowArray (ChangeArrayRows(createdArray, userRow1, userRow2));
+*/
+//Вариант 2 (с вводом строк которые меняем)
+
+int [,] Create2dArray (int rows, int columns, int min, int max)
+{
+    int [,] newArray = new int [rows, columns];
+    for (int i = 0; i < rows; i++)
+    {
+        for(int j = 0; j < columns; j++)
+            newArray[i, j] = new Random().Next(min, max+1);
+    }
+    return newArray;
+}
+
+void ShowArray(int [,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for(int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine(); 
+}
+
+int [,] ChangeArrayRows(int [,] array1, int row1, int row2)
+{
+    Console.WriteLine("Input number of 1st row for remove: ");
+    int userRow1 = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Input number of 2nd row for remove: ");
+    int userRow2 = Convert.ToInt32(Console.ReadLine());
+
+    if (row1 >= 0 && row1 < array1.GetLength(0) && row2 >= 0 && row2 < array1.GetLength(0) && row1 != row2)
+    {
+        for (int j = 0; j < array1.GetLength(1); j++)
+        {
+            int temp = array1[row1, j];
+            array1[row1, j] = array1[row2, j];
+            array1[row2, j] = temp;
+        }
+    return array1;
+    }
+    else 
+    {
+        Console.WriteLine("incorrect input!");
+        ChangeArrayRows(array1);
+    }
+}
+
+Console.WriteLine("Input numbers of rows: ");
+int userRows = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input numbers of column: ");
+int userColumns = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input min possible value: ");
+int userMinValue = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input max possible value: ");
+int userMaxValue = Convert.ToInt32(Console.ReadLine());
 
 int [,] createdArray = Create2dArray(userRows, userColumns, userMinValue, userMaxValue);
 ShowArray (createdArray);
